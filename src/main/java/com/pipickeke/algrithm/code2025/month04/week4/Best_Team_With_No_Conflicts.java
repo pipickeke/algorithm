6b52defdc38c1b3a056783b23f864eabfb0924f3
pipickeke <ycltony7398@163.com>
2025-04-30 17:49:29 +0800

    #2025-04-30 最长递增子序列

---
+++ src/main/java/com/pipickeke/algrithm/code2025/month04/week4/Best_Team_With_No_Conflicts.java
@@ -0,0 +1,41 @@
+package com.pipickeke.algrithm.code2025.month04.week4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 1626. 无矛盾的最佳球队
+ * 假设你是球队的经理。对于即将到来的锦标赛，你想组合一支总体得分最高的球队。球队的得分是球队中所有球员的分数 总和 。
+ *
+ * 然而，球队中的矛盾会限制球员的发挥，所以必须选出一支 没有矛盾 的球队。如果一名年龄较小球员的分数 严格大于 一名年龄较大的球员，则存在矛盾。同龄球员之间不会发生矛盾。
+ *
+ * 给你两个列表 scores 和 ages，其中每组 scores[i] 和 ages[i] 表示第 i 名球员的分数和年龄。请你返回 所有可能的无矛盾球队中得分最高那支的分数 。
+ *
+ *
+ */
+
+public class Best_Team_With_No_Conflicts {
+
+    public int bestTeamScore(int[] scores, int[] ages){
+        int N = scores.length;
+        Integer[] idx = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            idx[i] = i;
+        }
+        Arrays.sort(idx, (x,y) -> scores[x] != scores[y] ?
+                scores[x] - scores[y] : ages[x] - ages[y]);
+
+        int[] dp = new int[N];
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (ages[dp[j]] <= ages[dp[i]]){
+                    dp[i] = Math.max(dp[i], dp[j]);
+                }
+            }
+            dp[i] += scores[idx[i]];
+            ans =  Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+}
--- src/main/java/com/pipickeke/algrithm/code2025/month04/week4/Minimum_Operations_to_Halve_Array_Sum.java
+++ src/main/java/com/pipickeke/algrithm/code2025/month04/week4/Minimum_Operations_to_Halve_Array_Sum.java
@@ -4,6 +4,14 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.PriorityQueue;
 
+/**
+ * 题目：2208. 将数组和减半的最少操作次数
+ * 给你一个正整数数组 nums 。每一次操作中，你可以从 nums 中选择 任意 一个数并将它减小到 恰好 一半。（注意，在后续操作中你可以对减半过的数继续执行操作）
+ *
+ * 请你返回将 nums 数组和 至少 减少一半的 最少 操作数。
+ *
+ */
+
 public class Minimum_Operations_to_Halve_Array_Sum {
 
 
